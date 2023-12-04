3483929e7bf1ec57bf8477cdc3806254b7cd4361
Sam Gorbatov <sam.gorbatov@irwin.local>
2023-12-05 09:20:08 +1100

    Day 1 part 1

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+open System
+// For more information see https://aka.ms/fsharp-console-apps
+
+let convertToDigits(line: string) =
+
+    let charToNumber(ch : char) = int ch - int '0'
+    let digits = line |> Seq.where(fun x -> System.Char.IsDigit(x))
+    let firstDigit = digits |> Seq.head
+    let lastDigit = digits |> Seq.last
+
+    charToNumber(firstDigit) * 10 + charToNumber(lastDigit)
+
+let lines = File.ReadAllLines("./2023/Day1.txt")
+let sum = lines
+        |> Array.map(convertToDigits)
+        |> Array.sum
+        
+printf($"{sum}")
+
+
